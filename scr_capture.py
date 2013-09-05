b317d35f9a5176ec4965b2428580c0f8f1aec974
Pavel Nedrigaylov <pavel.nedr@gmail.com>
2013-09-05 11:47:47 +0400

    basic functionality

---
+++ scr_capture.py
@@ -0,0 +1,22 @@
+#!/bin/python
+# -*- coding: utf_8 -*-
+import subprocess, sys, os.path
+
+# get resolution
+res = subprocess.check_output("xrandr | grep \*", shell=True).split()[0]
+
+# ffmpeg command
+command = ("ffmpeg -f x11grab -r 30 -s {resolution} -i :0.0 -vcodec libx264 "
+           "-preset ultrafast -crf 0 -threads 0 {filename}")
+
+if len(sys.argv) <> 2:
+    print "You must specify filename to save, for example:"
+    print "./{my_name} screencast.mkv".format(my_name=__file__)
+    print "python {my_name} screencast.mkv".format(my_name=__file__)
+    sys.exit(1)
+
+sys.exit(subprocess.call(command.format(resolution=res, filename=sys.argv[1]),
+                    shell=True))  
+ 
+
+
